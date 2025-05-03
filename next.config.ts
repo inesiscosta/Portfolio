6001a8909da3924277dd3132204abff42b0de1e8
Inês Costa <94355111+inesiscosta@users.noreply.github.com>
2025-05-03 12:38:42 +0100

    chore: update dependencies and clean repo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
